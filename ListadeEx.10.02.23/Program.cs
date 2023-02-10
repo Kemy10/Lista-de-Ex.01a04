c0f6989bad382a0cb1f4430b003c414a8cc5b19a
Kemy10 <120021911+Kemy10@users.noreply.github.com>
2023-02-10 09:26:29 -0300

    Add files via upload

---
+++ ListadeEx.10.02.23/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListadeEx._10._02._23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex:1
+            /*
+            string nomes;
+
+            Console.WriteLine("Digite seu nome: ");
+            Console.ReadLine();
+            */
+
+            /*Ex: 2
+            double soma;
+            */
+
+            /*Ex: 3
+             int nmr;
+            */
+
+            //Ex: 4
+
+            string nome;
+            Console.WriteLine("Digite seu nome");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("O nome " + nome + " é legal");
+           
+        }
+    }
+}
